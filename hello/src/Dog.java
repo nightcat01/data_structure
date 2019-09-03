e156cb149cb7f10f30d145eb3815f68ab56fccf0
nightcat910@gmail.com <rnjs1rjf01!>
2019-09-03 13:31:00 +0900

    여태 한거

---
+++ hello/src/Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    public static void main(String[] args) {
+        String[] strArr = {"|\\_/|", "|q p|   /}", "( 0 )\"\"\"\\", "|\"^\"`    |", "||_/=\\\\__|"};
+        for(int i = 0; i < strArr.length; i++) {
+            System.out.println(strArr[i]);
+        }
+    }
+}
---
+++ hello/src/MyAnswer.java
@@ -0,0 +1,8 @@
+public class MyAnswer {
+    public static void main(String[] args) {
+        String id = "nightcat";
+        int[] answerArr = {2557, 10171, 10172, 10718};
+        System.out.println(answerArr.length);
+        System.out.println(id);
+    }
+}
